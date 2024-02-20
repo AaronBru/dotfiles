cb809dda548ce4e5ba222f7a0c1ac58c6f8b9a53
Aaron Bruinsma <aaron.t.bruinsma@gmail.com>
2024-02-20 08:11:38 -0500

    Plenty more updates.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Aaron Bruinsma"
+      user-mail-address "aaron.t.bruinsma@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -27,10 +27,33 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-vibrant)
 
-
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+;; (setq org-roam-directory "~/org/roam")
+;; (setq org-roam-dailies-directory "journals/")
+;; (setq org-roam-completion-anywhere t)
+;; (setq org-roam-capture-templates
+;;       '(("d" "default" plain
+;;          "%?" :target
+;;          (file+head "${slug}.org" "#+title: ${title}\n")
+;;          :unnarrowed t)
+;;         ("p" "Capture to project" plain
+;;          "%?" :target
+;;          (file+head "projects.org" "+title: ${title}\n"))))
+
+;; (defun my/org-roam-capture-inbox ()
+;;   (interactive)
+;;   (org-roam-capture- :node (org-roam-node-create)
+;;                      :templates '(("i" "inbox" plain "* %?"
+;;                                    :if-new (file+head "inbox.org" "#+title: Inbox\n"))))
+;; )
+
+;; (defun my/org-roam-capture-project ()
+;;   (interactive)
+;;   (org-roam-capture- :node (org-roam-node-create)
+;;                      :templates '(("i" "inbox" plain "* %?"
+;;                                    :if-new (file+head "inbox.org" "#+title: Inbox\n")))))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -54,16 +77,55 @@
 
 (setq vterm-module-cmake-args "-DUSE_SYSTEM_LIBVTERM=no")
 
+(defun run-in-vterm (command name)
+  (let ((buf (cond ((get-buffer name))
+                   (t (vterm name)))))
+     (with-current-buffer buf
+       (vterm-send-string command)
+       (vterm-send-return)))
+)
+
+(defun run-in-vterm-other-window (command name)
+  (let ((buf (cond ((get-buffer name))
+                   (t (vterm-other-window name)))))
+     (with-current-buffer buf
+       (vterm-send-string command)
+       (vterm-send-return)))
+)
+
+;; prepare the arguments
+(setq dotfiles-git-dir (concat "--git-dir=" (expand-file-name "~/.dotfiles")))
+(setq dotfiles-work-tree (concat "--work-tree=" (expand-file-name "~")))
+
+;; function to start magit on dotfiles
+(defun dotfiles-magit-status ()
+  (interactive)
+  (add-to-list 'magit-git-global-arguments dotfiles-git-dir)
+  (add-to-list 'magit-git-global-arguments dotfiles-work-tree)
+  (call-interactively 'magit-status))
+
+(map! :leader
+      ( :prefix "g"
+        :desc "dotfiles-magit-status" "P" #'dotfiles-magit-status))
+
+;; wrapper to remove additional args before starting magit
+(defun magit-status-with-removed-dotfiles-args ()
+  (interactive)
+  (setq magit-git-global-arguments (remove dotfiles-git-dir magit-git-global-arguments))
+  (setq magit-git-global-arguments (remove dotfiles-work-tree magit-git-global-arguments))
+  (call-interactively 'magit-status))
+;; redirect global magit hotkey to our wrapper
+(global-set-key [remap magit-status] 'magit-status-with-removed-dotfiles-args)
+
 ;; Let projectile find all files within a project without needing to have visited
 ;; them prior
-(setq projectile-indexing-method 'alien)
-(setq projectile-enable-caching nil)
-(setq projectile-require-project-root nil)
+(setq projectile-indexing-method 'hybrid)
+(setq projectile-enable-caching t)
+(after! projectile
+    (setq projectile-project-root-files-bottom-up (remove ".project" projectile-project-root-files-bottom-up))
+)
 
-(setq projectile-project-root-functions '(projectile-root-local
-                                          projectile-root-top-down
-                                          projectile-root-top-down-recurring
-                                          projectile-root-bottom-up))
+;(setq max-specpdl-size 2600)
 
 (map! :leader
       ( :prefix "l"
@@ -73,6 +135,8 @@
 ;; Some keybinds brought over from my time with vim+cscope+fzf
 (map! :desc "Jump to definition" :m "C-]" #'+lookup/definition)
 (map! :desc "Find file" :n "C-p" #'helm-find)
+
+(setq helm-findutils-search-full-path t)
 
 ;; Fix latex at some point...
 (setq org-latex-pdf-process '("latexmk -f -pdf -interaction=nonstopmode -output-directory=%o %f"))
@@ -90,7 +154,12 @@
       ( :prefix "c"
         :desc "Find compile command" "F" #'my/helm/compile/project))
 
-(load "~/.doom.d/build_cmds.el")
+(if (file-exists-p "~/.doom.d/build_cmds.el")
+    (load "~/.doom.d/build_cmds.el")
+)
+
+(setq docker-container-shell-file-name "/bin/bash")
+(setq vterm-tramp-shells '(("docker" "/bin/bash")))
 
 (use-package! ace-window
   :config
@@ -115,6 +184,15 @@
 
 (c-add-style "my-cc-style" my-cc-style)
 (add-hook 'c++-mode-hook (lambda () (c-set-style "my-cc-style") ))
+
+;; Load vterm immediately as docker features work differently if vterm is loaded after
+;; vterm is also not correctly loaded when docker is loaded
+(use-package! vterm
+  :demand t)
+
+(if (file-exists-p "~/.doom.d/config_local.el")
+    (load "~/.doom.d/config_local.el")
+)
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
